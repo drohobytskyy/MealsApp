bc1350c141861e3a3d06a4cb7648c79295a5c625
@rtur drohobytskyy <artur_drohobytskyy@macbook-pro.lan>
2019-11-30 17:26:56 +0000

    Initial Commit

---
+++ App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  App
+//
+//  Created by @rtur drohobytskyy on 30/11/2019.
+//  Copyright © 2019 @rtur drohobytskyy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
